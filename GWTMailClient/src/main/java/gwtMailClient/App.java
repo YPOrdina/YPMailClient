37ff6edd5bbbb0bedc9735c9ce384f14231801bd
nielsmaneschijn <niels@fmf.nl>
2014-09-09 16:57:57 +0200

    pak aan

--- GWTMailClient/src/main/java/gwtMailClient/App.java
+++ GWTMailClient/src/main/java/gwtMailClient/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello MailClient!!" );
         System.out.println("check push");
+        System.out.println('Ik ben Niels! Mwoehaha');
     }
 }
