f5a3ebd5b4d9131be65279f3f5e47a1e2fa3757e
soz20659 <soz20659@BA91-CND4232Q0H.work.local>
2014-09-09 17:15:30 +0200

    ben ik niet

--- GWTMailClient/src/main/java/gwtMailClient/App.java
+++ GWTMailClient/src/main/java/gwtMailClient/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello MailClient!!" );
         System.out.println("check push");
         System.out.println("Ik ben Niels! Mwoehaha");
+        System.out.println("ozil");
     }
 }
