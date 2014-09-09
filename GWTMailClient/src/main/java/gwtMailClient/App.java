adf1523f3b6916e06bb689779b282f6158df282e
soz20659 <soz20659@BA91-CND4232Q0H.work.local>
2014-09-09 17:30:32 +0200

    push

---
+++ GWTMailClient/src/main/java/gwtMailClient/App.java
@@ -0,0 +1,15 @@
+package gwtMailClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello MailClient!!" );
+        System.out.println("check push");
+        System.out.println("Ik ben Niels! Mwoehaha");
+    }
+}
